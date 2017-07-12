fb0252d56e68fe263f859b318fd523143103145d
Victor Igor <victor.0w3@hotmail.com>
2017-07-12 10:38:58 -0300

    git initial

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp'),
+    koutoSwiss = require('kouto-swiss'),
+    stylus = require('gulp-stylus'),
+    browserSync = require('browser-sync').create(),
+    htmlmin = require('gulp-htmlmin'),
+    clean = require('gulp-clean'),
+    runSequence = require('run-sequence'),
+    reload = browserSync.reload,
+    jeet = require('jeet');
+
+gulp.task('clean', function() {
+	return gulp.src('dist/')
+		.pipe(clean());
+});
+
+/*Stylus task*/
+gulp.task('stylus', function() {
+  gulp.src('styl/main.styl')
+    .pipe(stylus({
+      use: [koutoSwiss(), jeet()],
+      compress: true
+    }))
+    .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('htmlmin', function() {
+  return gulp.src('./*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  })
+})
+
+gulp.task('watch', function () {
+	gulp.watch('styl/**/*.styl', ['stylus']).on('change', reload);
+	gulp.watch('*.html', ['htmlmin']).on('change', reload);
+});
+
+gulp.task('default', function(cb) {
+  return runSequence('clean', ['stylus', 'htmlmin', 'watch',  'serve'], cb);
+});
