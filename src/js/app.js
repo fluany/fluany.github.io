914597f4b20deb857646a3b934af476bc43da348
Victor Igor <victor.0w3@hotmail.com>
2017-10-10 23:54:18 -0300

    add link to download extension

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,13 @@
 (function(){
   var downloadButton = document.querySelectorAll('.btn-download');
   downloadButton.forEach(function(element) {
-    element.addEventListener('click', function() {
-      ga('send', 'event', 'link', 'click', 'Download');
-    });
+    if (chrome.app.isInstalled) {
+      element.style.display = 'none';
+    }else{
+      element.addEventListener('click', function() {
+        ga('send', 'event', 'link', 'click', 'Download');
+        chrome.webstore.install()
+      });
+    }
   })
 })()
